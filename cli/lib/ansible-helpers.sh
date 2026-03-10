76146b5cfdae0d531ef376220d19b593064bcdc1
zaheer <zaheer.hattiwale@quodroid.com>
2026-03-10 16:18:45 +0530

    Add Dodo Payments environment variable support to Ansible deployment
    - Updated ansible-helpers.sh to read Dodo Payments environment variables
      - DODO_PAYMENTS_API_KEY
      - DODO_WEBHOOK_SECRET
      - DODO_PRODUCT_ID_PRO
      - DODO_PRODUCT_ID_TEAM
      - DODO_PRODUCT_ID_BUSINESS
    - Pass Dodo variables as Ansible extra vars during deployment
    - Added Ansible task to populate Dodo credentials in .env files
    - Variables will be written to .env.dev or .env.prod during deployment
    
    Related: Dodo Payments billing integration automation

--- cli/lib/ansible-helpers.sh
+++ cli/lib/ansible-helpers.sh
@@ -77,6 +77,13 @@ ansible_deploy() {
     # Get Claude credentials from environment (set by deployment workflow)
     local claude_creds="${CLAUDE_CREDENTIALS_JSON:-}"
 
+    # Get Dodo Payments credentials from environment (set by deployment workflow)
+    local dodo_api_key="${DODO_PAYMENTS_API_KEY:-}"
+    local dodo_webhook_secret="${DODO_WEBHOOK_SECRET:-}"
+    local dodo_product_id_pro="${DODO_PRODUCT_ID_PRO:-}"
+    local dodo_product_id_team="${DODO_PRODUCT_ID_TEAM:-}"
+    local dodo_product_id_business="${DODO_PRODUCT_ID_BUSINESS:-}"
+
     ANSIBLE_ROLES_PATH="$DEPLOYER_ROOT/ansible/roles" ansible-playbook \
         -i "$inventory_file" \
         "$playbook" \
@@ -94,7 +101,12 @@ ansible_deploy() {
         -e "django_allowed_hosts=$allowed_hosts" \
         -e "cdn_domain=$cdn_domain" \
         -e "test_executor=$test_executor" \
-        -e "claude_credentials_json=$claude_creds"
+        -e "claude_credentials_json=$claude_creds" \
+        -e "dodo_api_key=$dodo_api_key" \
+        -e "dodo_webhook_secret=$dodo_webhook_secret" \
+        -e "dodo_product_id_pro=$dodo_product_id_pro" \
+        -e "dodo_product_id_team=$dodo_product_id_team" \
+        -e "dodo_product_id_business=$dodo_product_id_business"
 
     cd "$WORK_DIR"
 
